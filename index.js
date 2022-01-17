f68c7f0589ed5e6632df7dca65bf1148f2b5ab48
Misheelbat <p_misheel@me.com>
2022-01-17 18:12:35 +0100

    added frontend

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const morgan = require("morgan");
 app.use(cors());
 app.use(express.json());
+app.use(express.static("build"));
 
 app.use(
   morgan(function (tokens, req, res) {
@@ -45,7 +46,7 @@ let persons = [
 // get homepage
 app.get("/", (request, response) => {
   response.send("<h1>Hello World</h1>");
-}); 
+});
 
 // get all persons
 app.get("/api/persons", (request, response) => {
