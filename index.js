f92b455b153f1c9f62928873ccbde3f89f2cef53
Misheelbat <p_misheel@me.com>
2022-01-17 15:47:59 +0100

    done 3.7-3.8

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const { request } = require("express");
 const express = require("express");
 const nodemon = require("nodemon");
+const morgan = require("morgan");
 const app = express();
 app.use(express.json());
+
+app.use(
+  morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, "content-length"),
+      "-",
+      tokens["response-time"](req, res),
+      "ms",
+      JSON.stringify(req.body),
+    ].join(" ");
+  })
+);
 let persons = [
   {
     id: 1,
